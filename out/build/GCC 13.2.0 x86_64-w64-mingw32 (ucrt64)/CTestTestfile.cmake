de077ef1e16cc4424a8908ca8c42d5325824850c
Leerexter <tleeflem@gmail.com>
2024-08-19 15:37:11 +0200

    cmake init

---
+++ out/build/GCC 13.2.0 x86_64-w64-mingw32 (ucrt64)/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/CRVW
+# Build directory: C:/CRVW/out/build/GCC 13.2.0 x86_64-w64-mingw32 (ucrt64)
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
